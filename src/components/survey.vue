669d0652f47a8a9bf26ea666dccb119269dd4537
Daniel <mwdchang@gmail.com>
2018-03-17 11:22:48 -0400

    random position

--- src/components/survey.vue
+++ src/components/survey.vue
@@ -3,20 +3,24 @@
     <div class="container">
       <svg class="canvas"></svg>
     </div>
-    <div class="button">Submit</div>
+    <div class="button" @click="submit">Submit</div>
   </div>
 </template>
 
 <script>
+
+const H = 500;
+const W = 500;
+
 export default {
   name: 'survey',
   data() {
     return {
       players: [
-        {id: 1},
-        {id: 2},
-        {id: 3},
-        {id: 4}
+        {id: 1, x: 0, y: 0},
+        {id: 2, x: 0, y: 0},
+        {id: 3, x: 0, y: 0},
+        {id: 4, x: 0, y: 0}
       ],
       xaxis: ['Underrated', 'Overrated'],
       yaxis: ['Replacement-level', 'Hall worthy']
@@ -28,15 +32,31 @@ export default {
     this.createScenario();
   },
   methods: {
+    submit() {
+      const data = this.canvas.selectAll('.avatar').data();
+      console.log('submitting', data);
+    },
     createScenario() {
       const testImg = 'https://d3k2oh6evki4b7.cloudfront.net/req/201802130/images/headshots/e/e31675e7_davis.jpg';
+
+      this.players.forEach( player => {
+        player.x = Math.random()*W;
+        player.y = Math.random()*H;
+      });
+
       function dragstarted(d) {
       }
-      
       function dragged(d) {
-        d3.select(this).attr("x", d.x = d3.event.x).attr("y", d.y = d3.event.y);
+        d3.select(this)
+          .attr("x", () => {
+            d.x = d3.event.x;
+            return d.x;
+          })
+          .attr("y", () => {
+            d.y = d3.event.y;
+            return d.y;
+          });
       }
-      
       function dragended(d) {
       }
 
@@ -53,8 +73,8 @@ export default {
         .attr('xlink:href', testImg)
         .attr('width', 50)
         .attr('height', 50)
-        .attr('x', (d, i) => i*100)
-        .attr('y', (d, i) => i*100)
+        .attr('x', (d, i) => d.x)
+        .attr('y', (d, i) => d.y)
         .call(drag);
     },
     initialize() {
@@ -64,19 +84,42 @@ export default {
       this.canvas.append('rect')
         .attr('x', 0)
         .attr('y', ymid)
-        .attr('width', 500)
+        .attr('width', W)
         .attr('height', 1)
         .style('stroke', null)
-        .style('fill', '#BBB');
+        .style('fill', '#CCC');
 
       this.canvas.append('rect')
         .attr('x', xmid)
         .attr('y', 0)
         .attr('width', 1)
-        .attr('height', 500)
+        .attr('height', H)
         .style('stroke', null)
-        .style('fill', '#BBB');
-      }
+        .style('fill', '#CCC');
+      
+      this.canvas.append('text')
+        .attr('x', 0)
+        .attr('y', ymid)
+        .text(this.xaxis[0]);
+
+      this.canvas.append('text')
+        .attr('x', W)
+        .attr('y', ymid)
+        .style('text-anchor', 'end')
+        .text(this.xaxis[1]);
+
+      this.canvas.append('text')
+        .attr('x', xmid)
+        .attr('y', 15)
+        .style('text-anchor', 'middle')
+        .text(this.yaxis[1]);
+
+      this.canvas.append('text')
+        .attr('x', xmid)
+        .attr('y', H-5)
+        .style('text-anchor', 'middle')
+        .text(this.yaxis[0]);
+    }
   }
 }
 </script>
@@ -100,7 +143,6 @@ export default {
   border: 1px solid #444;
 }
 
-
 .button:hover {
   background: #ddd;
   cursor: pointer;
@@ -116,6 +158,5 @@ svg {
   width: 30;
   height: 30;
 }
-
 </style>
 
